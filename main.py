62b2c84ef13143997ee2d342021a7f159de571bd
jetsalinas <joseenrico.salinas@gmail.com>
2018-02-21 03:12:21 +0800

    Added csv loader function

--- main.py
+++ main.py
@@ -2,9 +2,33 @@ from scipy.stats import *
 import numpy
 import csv
 
+import os
 
-def csv_debug():
-	print("Hello World")
+
+csv_directory = os.path.join("data-formatted")
+
+def load_csv(name = None):
+	csvfiles = []
+	if name is None:
+		names = os.listdir(csv_directory)
+		try:
+			for name in names:
+				csvfiles.append(open("{0}/{1}".format(csv_directory, name), mode='r'))
+				print("Found csv files: " + names.__str__())
+		except FileNotFoundError:
+			print("No csv files found")
+			return None
+	else:
+		try:
+			csvfiles.append(open("{0}/{1}".format(csv_directory, name), mode='r'))
+			print("Found csv files: " + name)
+		except:
+			print("No csv file found")
+			return None
+	return csvfiles
+
+def debug():
+	print("Debug function running")
 
 if __name__ == "__main__":
-	csv_debug()
+	load_csv()
