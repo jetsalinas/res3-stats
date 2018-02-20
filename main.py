c21e1c3c050b06debfdf6ee5ed950693173d65c3
jetsalinas <joseenrico.salinas@gmail.com>
2018-02-21 03:28:06 +0800

    Added csv debugger function

--- main.py
+++ main.py
@@ -5,30 +5,41 @@ import csv
 import os
 
 
+def debug():
+	print("Debug function running.")
+
 csv_directory = os.path.join("data-formatted")
 
 def load_csv(name = None):
-	csvfiles = []
+	csv_files = []
 	if name is None:
 		names = os.listdir(csv_directory)
 		try:
 			for name in names:
-				csvfiles.append(open("{0}/{1}".format(csv_directory, name), mode='r'))
-				print("Found csv files: " + names.__str__())
+				csv_files.append(open("{0}/{1}".format(csv_directory, name), mode='r'))
+			print("Found csv files: " + names.__str__())
 		except FileNotFoundError:
 			print("No csv files found")
 			return None
 	else:
 		try:
-			csvfiles.append(open("{0}/{1}".format(csv_directory, name), mode='r'))
+			csv_files.append(open("{0}/{1}".format(csv_directory, name), mode='r'))
 			print("Found csv files: " + name)
 		except:
 			print("No csv file found")
 			return None
-	return csvfiles
+	return csv_files
 
-def debug():
-	print("Debug function running")
+def debug_csv(lines = 2):
+	csv_files = load_csv()
+	for i in csv_files:
+		f = csv.reader(i)
+		for j in range(lines):
+			print(", ".join(f.__next__()))
+		print()
+
+def run_stats():
+	csv_files = load_csv()
 
 if __name__ == "__main__":
-	load_csv()
+	run_stats()
