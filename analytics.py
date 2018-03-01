640a4d39000eca7a5a8d00448682c995f2132e1d
jetsalinas <joseenrico.salinas@gmail.com>
2018-03-01 23:27:31 +0800

    Formatted analytics.py output

--- analytics.py
+++ analytics.py
@@ -9,11 +9,22 @@ formatted_data_directory = os.path.join("data-formatted/")
 
 def get_data():
 	data = pd.read_csv("{0}{1}".format(formatted_data_directory, "indicators.csv"))
-	#headers: country_name country_code indicator_name indicator_code year value
+	brn = data[(data['country_code'] == 'BRN')]
+	khm = data[(data['country_code'] == 'KHM')]
+	idn = data[(data['country_code'] == 'IDN')]
+	lao = data[(data['country_code'] == 'LAO')]
+	mys = data[(data['country_code'] == 'MYS')]
+	mmr = data[(data['country_code'] == 'MMR')]
+	phl = data[(data['country_code'] == 'PHL')]
+	sgp = data[(data['country_code'] == 'SGP')]
+	tha = data[(data['country_code'] == 'THA')]
+	vnm = data[(data['country_code'] == 'VNM')]
+
+	data = {"BRN": brn, "KHM": khm, "IDN": idn, "LAO": lao, "MYS": mys, "MMR": mmr, "PHL": phl, "SGP": sgp, "THA": tha, "VNM": vnm}
 	return data
 
 def run():
-	analyze()
+	pass
 
 if __name__ == "__main__":
 	run()
