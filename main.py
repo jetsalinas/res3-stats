dd0efd61344996eba6e2a0d81e682ec4b42629fc
jetsalinas <joseenrico.salinas@gmail.com>
2018-03-01 21:13:18 +0800

    File cleanup

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib as mpl
+
 import preprocessor
 import project
+
+def run_analytics():
 
 if __name__ == '__main__':
 	project.run()
